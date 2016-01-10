7187b76b3745d3ff651bff9aff333b2e6bd72414
hdknr <gmail@hdknr.com>
2016-01-11 06:50:01 +0900

    Signin page

---
+++ DroidLayout/SigninActivity.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DroidLayout
+{
+	[Activity (Label = "SigninActivity")]			
+	public class SigninActivity : Activity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{	
+			base.OnCreate (savedInstanceState);
+
+			// Create your application here
+
+			SetContentView (Resource.Layout.Signin);
+
+			SignInButton.Click += (object sender, EventArgs e) => {
+				Finish();
+			};			
+		}
+
+		Button SignInButton {
+			get { return FindViewById<Button>(Resource.Id.btnSingIn);}}
+		
+	}
+}
+
