aaa089c74e5a5ed48623a015579f4d679e1892eb
hdknr <gmail@hdknr.com>
2016-01-11 13:37:46 +0900

    Create and run a Timer OnCreate() , dispose it when Finishing

--- DroidLayout/SigninActivity.cs
+++ DroidLayout/SigninActivity.cs
@@ -11,20 +11,33 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using Android.Util;		//Log
+
+using System.Threading;	//Timer
+
 namespace DroidLayout
 {
 	[Activity (Label = "SigninActivity")]			
 	public class SigninActivity : Activity
 	{
+		int interval = 1000;
+
 		protected override void OnCreate (Bundle savedInstanceState)
 		{	
+			Log.Debug (this.LocalClassName, "OnCreate");
+
 			base.OnCreate (savedInstanceState);
 
 			// Create your application here
 
 			SetContentView (Resource.Layout.Signin);
 
+			var timer = new Timer((o) => RunOnUiThread(() =>{
+				Console.WriteLine(DateTime.Now.ToLongTimeString());
+			}), null , 0, interval);	
+
 			SignInButton.Click += (object sender, EventArgs e) => {
+				timer.Dispose();
 				Finish();
 			};			
 		}
